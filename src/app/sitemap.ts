2ca07c26864afc065a7d72507eac588fa005deea
tombo <tombo.zoltan@gmail.com>
2025-12-14 00:35:22 +0100

    Add force-static to sitemap and robots

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,6 +2,8 @@ import type { MetadataRoute } from "next";
 
 import { blogPosts } from "@/lib/blog-data";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = "https://e-marketplace.hu";
 
